08000ee0a1f1cc98428b6342f730b0848c80e54e
seiyanbelltree <oasis0611@keio.jp>
2020-02-10 02:56:38 +0900

    try to deploy with herocku

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -129,3 +129,20 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
