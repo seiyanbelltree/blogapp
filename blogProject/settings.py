c3e71b976521e270448766a4dd588e65bbafce98
seiyanbelltree <oasis0611@keio.jp>
2020-02-11 18:04:31 +0900

    change static root

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://8080-dot-9893470-dot-devshell.appspot.com/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'deploy')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
