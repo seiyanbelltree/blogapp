e274cddcb9c34e1c67f7d37e1f7c1cfc8d2dae8e
seiyanbelltree <oasis0611@keio.jp>
2020-04-19 18:36:27 +0900

    change model.py

--- blogApp/models.py
+++ blogApp/models.py
@@ -13,13 +13,6 @@ class Image(models.Model):
     image = models.ImageField(upload_to='photos')
     path = models.CharField(max_length=200, null=True)
 
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-
     def publish(self):
         self.published_date = timezone.now()
         self.save()
